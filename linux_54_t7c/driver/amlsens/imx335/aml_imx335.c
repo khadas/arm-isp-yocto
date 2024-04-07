40409af4c7e7893ad519f7d00303ff59f6acfbc9
zhiwei.zhang <zhiwei.zhang@amlogic.com>
2024-04-07 14:10:46 +0800

    camera: imx335 support [2/2]
    PD#SWPL-163904
    
    Problem:
    need imx335 sdr support.
    
    Solution:
    update imx335 driver. MSB at high address.
    
    Verify:
    local on an400 board.
    
    Change-Id: I9ea390feb01c0d5e80bd446d6b665a6c915c5953
    Signed-off-by: zhiwei.zhang <zhiwei.zhang@amlogic.com>

--- linux_54_t7c/driver/amlsens/imx335/aml_imx335.c
+++ linux_54_t7c/driver/amlsens/imx335/aml_imx335.c
@@ -116,7 +116,7 @@ static int imx335_write_buffered_reg(struct imx335 *imx335, u16 address_low,
 	}
 
 	for (i = 0; i < nr_regs; i++) {
-		val = (u8)(value >> ((nr_regs - 1 - i) * 8));
+		val = (u8)(value >> (i * 8));
 		ret = imx335_write_reg(imx335, address_low + i, val);
 		if (ret) {
 			dev_err(imx335->dev, "Error writing buffered registers\n");
@@ -150,7 +150,7 @@ static int imx335_set_exposure(struct imx335 *imx335, u32 value)
 
 	ret = imx335_write_buffered_reg(imx335, IMX335_EXPOSURE, 2, value & 0xFFFF);
 	if (ret)
-		dev_err(imx335->dev, "Unable to write gain\n");
+		dev_err(imx335->dev, "Unable to write expo\n");
 
 	return ret;
 }
